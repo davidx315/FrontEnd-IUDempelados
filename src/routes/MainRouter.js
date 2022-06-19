47d2cf4e531e4f53782cfe65bef6765b29df936a
davidx315 <david315cg@hotmail.com>
2022-06-19 10:33:52 -0500

    first commit

---
+++ src/routes/MainRouter.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Estado from '../components/estado/Estado'
+import NavBar from '../components/interfaceUsuario/NavBar'
+import Inventario from '../components/inventario/Inventario'
+import Marca from '../components/marca/Marca'
+import TipoEquipo from '../components/tipoEquipo/TipoEquipo'
+import Usuario from '../components/usuario/Usuario'
+
+export default function MainRouter() {
+  return (
+    <BrowserRouter>
+        <NavBar />
+        <Routes>
+            <Route  path='/' element={<Estado />}/>
+            <Route  path='/marcas' element={<Marca />}/>
+            <Route  path='/tipoequipos' element={<TipoEquipo />}/>
+            <Route  path='/inventarios' element={<Inventario />}/>
+            <Route  path='/usuarios' element={<Usuario />}/>
+        </Routes>
+    </BrowserRouter>
+  )
+}
